0bab9e97221e09d05e44bfc3595f248df8810f73
Daniel <dvega@cellit.cl>
2025-06-07 16:39:52 -0400

    fix: configuration cors added

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const authRoutes = require('./routes/auth');
 const protectedRoutes = require('./routes/protected');
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors()); // Preflight
+
 app.use(express.json());
 
 app.use('/api', authRoutes);
@@ -15,4 +22,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
